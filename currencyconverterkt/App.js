e0764daf3c673f9bfcef9ffade2e9153463d4281
Eriitunu Adesioye <eriitunuadesioye@yahoo.com>
2017-10-24 15:20:05 +0100

    Finished adding new container to main application

--- currencyconverterkt/App.js
+++ currencyconverterkt/App.js
@@ -1,23 +1,28 @@
 import React from 'react';
-import {  Text, View } from 'react-native';
+import { Text, View } from 'react-native';
+import GenericCurrencyField from './src/components/GenericCurrencyField';
+
 
 export default class App extends React.Component {
+
+
   render() {
     return (
-      <View style={viewStylestyles.container}>
-{// this view represents the header Area}
-        <View style={viewStyle.container}>
-        <View style ={viewStylestyles.headerArea}>
+      <View style={viewStyles.container}>
+
+        {/* this view represents the header Area */}
+        <View style ={viewStyles.headerArea}>
           <Text style={textStyles.header}> CURRENCY CONVERTER</Text>
         </View>
 
         <View style={viewStyles.currencyBlockBackground}>
+          <GenericCurrencyField />
         </View>
-
 
       </View>
     );
   }
+
 }
 
 const viewStyles = {
@@ -26,30 +31,28 @@ const viewStyles = {
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'flex-start',
-    paddingTop: '15',
+    paddingTop: 15,
     flexDirection: 'column',
   },
   //the style of the header area for the app
-  headerArea:  {
-    backgroundColor: '',
+  headerArea: {
     width: '100%',
     height: '12%',
     justifyContent: 'center',
     alignItems: 'center',
-
-    }
-
-    currencyBlockBackground: {
-      backgroundColor: '#252525',
-      width: '100%',
-      height: '30%',
-    }
-};
-const textStyles = {
-  header:{
-    fontSize:'17',
-    fontWeight: 'bold',
-
+  },
+  currencyBlockBackground: {
+    backgroundColor: '#252525',
+    width: '100%',
+    height: '30%',
   }
 
+};
+
+
+const textStyles = {
+  header:{
+    fontSize: 17        ,
+    fontWeight: 'bold',
+  }
 };
--- currencyconverterkt/src/components/GenericCurrencyField.js
+++ currencyconverterkt/src/components/GenericCurrencyField.js
@@ -4,20 +4,34 @@ import {Text, Image, TouchableHighlight, View} from 'react-native';
 
 class GenericCurrencyField extends Component {
 
-construcor(props){
-  // this means it will go to thr parent component and execute the code in the PARENT CCOMPONENTS'S constructor
-  super(props);
-}
-// render has to be invoked if the component is to be drawn on the screen.
-  render(){
-    return (
-      <View>
+  constructor(props){
+    // this means it will go to thr parent component and execute the code in the PARENT CCOMPONENTS'S constructor
+    super(props);
+  }
 
+  // render has to be invoked if the component is to be drawn on the screen.
+  render(){
+
+    return (
+      <View style={viewStyles.container}>
 
       </View>
 
     );
+
   }
+
+
 }
+
+const viewStyles = {
+
+    container: {
+        backgroundColor: 'lightblue',
+        width: '100%',
+        height: 100
+    }
+
+};
 // this line of code allows the class to be accesible in other parts of the app (in other files/ classes)
 export default GenericCurrencyField;
